3d37eabef86a1ca937d2d87ae29a396f5c1b2a7f
Eliam <Texsword76@gmail.com>
2025-02-04 16:59:46 +0100

    ajout d'un Makefile et d'un fichier .gitignore, réorganisation de megaphone.cpp

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,16 +6,18 @@
 /*   By: edetoh <edetoh@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 12:02:09 by edetoh            #+#    #+#             */
-/*   Updated: 2025/02/04 13:33:20 by edetoh           ###   ########.fr       */
+/*   Updated: 2025/02/04 16:55:52 by edetoh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <cstdio>
+#include <iostream>
+#include <cctype>
 
 int main (int ac, char **av)
 {
 	int i = 0;
 	int j = 0;
+
 	if (ac > 1)
 	{
 		while (i < ac - 1)
@@ -23,23 +25,14 @@ int main (int ac, char **av)
 			j = 0;
 			while (av[i + 1][j])
 			{
-				if (av[i + 1][j] >= 'a' && av[i + 1][j] <= 'z')
-				{
-					printf("%c", (av[i + 1][j]) - 32);
-				}
-				else
-				{
-					printf("%c", (av[i + 1][j]));
-				}
+				std::cout << (char)std::toupper(av[i + 1][j]);
 				j++;
 			}
 			i++;
 		}
 	}
 	else
-	{
-		printf("* LOUD AND UNBEARABLE FEEDBACK NOISE *");
-	}
-	printf("\n");
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::cout << std::endl;
 	return (0);
 }

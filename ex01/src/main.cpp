973b8f13b6d25dc2407cd57abdbb5f41a05ba413
Eliam <Texsword76@gmail.com>
2025-03-11 11:31:26 +0100

    ça marche :)

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -24,30 +24,30 @@ int main()
 
 	while (1)
 	{
-		std::cout << "Enter a command (ADD, SEARCH, EXIT): ";
+		std::cout << ">> Enter a command (ADD, SEARCH, EXIT): ";
 		std::cin >> command;
 
 		if (command == "ADD")
 		{
 			Contact newContact;
 
-			std::cout << "Enter first name: ";
+			std::cout << "> Enter first name: ";
 			std::getline(std::cin >> std::ws, first_name);
 			newContact.set_first_name(first_name);
 
-			std::cout << "Enter last name: ";
+			std::cout << "> Enter last name: ";
 			std::getline(std::cin >> std::ws, last_name);
 			newContact.set_last_name(last_name);
 
-			std::cout << "Enter nickname: ";
+			std::cout << "> Enter nickname: ";
 			std::getline(std::cin >> std::ws, nickname);
 			newContact.set_nickname(nickname);
 
-			std::cout << "Enter darkest secret: ";
+			std::cout << "> Enter darkest secret: ";
 			std::getline(std::cin >> std::ws, darkest_secret);
 			newContact.set_darkest_secret(darkest_secret);
 
-			std::cout << "Enter phone number: ";
+			std::cout << "> Enter phone number: ";
 			std::getline(std::cin >> std::ws, phone_number);
 			newContact.set_phone_number(phone_number);
 
